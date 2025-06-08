6769fa472ad3dfd47b3cfaaa5d7cc5e3f3858b7c
yugal07 <yashsadhwani544@gmail.com>
2025-06-09 00:50:25 +0530

    Invert Binary Tree Java

--- Solutions/InvertTree.java
+++ Solutions/InvertTree.java
@@ -22,15 +22,15 @@ package Solutions;
 
 public class InvertTree {
     public static TreeNode invertTree(TreeNode root){
-        if(root == null) {
-            return root;
-        }
+        if (root == null) return root;
+
         TreeNode temp = root.left;
         root.left = root.right;
         root.right = temp;
 
         invertTree(root.left);
         invertTree(root.right);
+
         return root;
     }
 }
