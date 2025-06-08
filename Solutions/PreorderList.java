6c6a9c0ab6234f30788b94d84124e9e0c47f71e1
yugal07 <yashsadhwani544@gmail.com>
2025-06-08 23:12:14 +0530

    Preorder List using root

---
+++ Solutions/PreorderList.java
@@ -0,0 +1,62 @@
+package Solutions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Given the root of a binary tree, return the preorder traversal of its nodes' values.
+
+ 
+
+// Example 1:
+
+// Input: root = [1,null,2,3]
+
+// Output: [1,2,3]
+
+// Explanation:
+
+
+
+// Example 2:
+
+// Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+
+// Output: [1,2,4,5,6,7,3,8,9]
+
+// Explanation:
+
+
+
+// Example 3:
+
+// Input: root = []
+
+// Output: []
+
+// Example 4:
+
+// Input: root = [1]
+
+// Output: [1]
+
+ 
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [0, 100].
+// -100 <= Node.val <= 100
+
+public class PreorderList {
+    public static List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        preorderHelper(root, result);
+        return result;
+    }
+    public static void preorderHelper(TreeNode root , List<Integer> result){
+        if(root == null) return;
+
+        result.add(root.val);
+        preorderHelper(root.left, result);
+        preorderHelper(root.right, result);
+    }
+}
